ca2aae4b8f180e4b4242a561bf40dd7f787d9b23
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-06-13 20:21:42 +0200

    feat: costruttore

---
+++ src/main/java/org/snacks/java/Studente.java
@@ -0,0 +1,24 @@
+package org.snacks.java;
+
+  class Studente {
+	
+	private String firstName;
+	private String lastName;
+	private int age;
+	
+	private Studente(String firstName, String lastName, int age) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		
+		
+		
+	}
+	
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
