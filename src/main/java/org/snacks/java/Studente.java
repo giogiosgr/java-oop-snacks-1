01ae0d92f0eb6be45d52b1e3081282cb0efdcc71
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-06-13 22:36:34 +0200

    feat: classe Studente

--- src/main/java/org/snacks/java/Studente.java
+++ src/main/java/org/snacks/java/Studente.java
@@ -6,27 +6,21 @@ package org.snacks.java;
 	private String lastName;
 	private int age;
 	
-	private Studente(String firstName, String lastName, int age) {
+	Studente(String firstName, String lastName, int age) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.age = age;
 		
 	}
 	
-	private String getAllData() {
-		return this.firstName + " " + this.lastName + ", " + this.age;
-		
+	String getAllData() {
+		return this.firstName + " " + this.lastName + ", " + this.age + " anni";
 	}
 	
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		Studente studente = new Studente ("Ajeje", "Brazorf", 33);
 		System.out.println(studente.getAllData());
-			
-			
-		
-
 	}
 
 }
