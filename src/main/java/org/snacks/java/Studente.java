5a269ad47837de4c37db4b8cd4aeea06a77641c9
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-06-13 20:25:26 +0200

    completamento

--- src/main/java/org/snacks/java/Studente.java
+++ src/main/java/org/snacks/java/Studente.java
@@ -11,12 +11,20 @@ package org.snacks.java;
 		this.lastName = lastName;
 		this.age = age;
 		
-		
+	}
+	
+	private String getAllData() {
+		return this.firstName + " " + this.lastName + ", " + this.age;
 		
 	}
 	
 
 	public static void main(String[] args) {
+		
+		Studente studente = new Studente ("Ajeje", "Brazorf", 33);
+		System.out.println(studente.getAllData());
+			
+			
 		
 
 	}
