5a5ee2d8b638550741e939d4fd8e96d937950ed4
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-06-29 10:26:10 +0200

    feat: snack5

---
+++ src/main/java/org/snacks/java/Snack5.java
@@ -0,0 +1,29 @@
+package org.snacks.java;
+
+import java.util.*;
+
+public class Snack5 {
+	
+	static HashMap<String, String> elementi = new HashMap<String, String>();
+
+	public static void main(String[] args) {
+		
+		
+		elementi.put("andrea", "presente");
+		elementi.put("giacomo", "assente");
+		elementi.put("giulia", "assente");
+		elementi.put("francesco", "presente");
+		
+		
+		
+		checkChiaveDuplicata("francesco");
+
+	}
+	
+	public static void checkChiaveDuplicata (String key) {
+		
+		
+		
+	}
+
+}
